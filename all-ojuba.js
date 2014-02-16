bb71fac35ad388f63eb3241e8212c47db8a0e219
Mosaab Alzoubi <moceap@hotmail.com>
2014-02-16 21:26:30 +0300

    Update all-ojuba.js
    تفعيل التّمرير بزر الفأرة الاوسط

--- all-ojuba.js
+++ all-ojuba.js
@@ -1,3 +1,4 @@
 user_pref("browser.startup.homepage", "file:///usr/share/doc/HTML/index.html"); 
 pref("browser.tabs.autoHide",false);
 pref("network.dns.disableIPv6", true);
+pref("general.autoScroll", true);
