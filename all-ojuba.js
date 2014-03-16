0f682ce735d60091a91224ba1c7a2639906422e6
Mosaab Alzoubi <moceap@hotmail.com>
2014-03-16 21:40:24 +0300

    محاولة
    محاولة غير ناجحة لتفعيل علامات أعجوبة !!

--- all-ojuba.js
+++ all-ojuba.js
@@ -2,3 +2,4 @@ user_pref("browser.startup.homepage", "http://ojuba.org");
 pref("browser.tabs.autoHide",false);
 pref("network.dns.disableIPv6", true);
 pref("general.autoScroll", true);
+pref("browser.bookmarks.restore_default_bookmarks", true);
